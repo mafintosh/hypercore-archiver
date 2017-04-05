7bf2cdd85b3e32b6ff14e3154b86e056273ee067
karissa <krmckelv@gmail.com>
2017-04-05 14:43:04 -0700

    update tests

--- tests/feed.js
+++ tests/feed.js
@@ -13,9 +13,9 @@ test('prep', function (t) {
 })
 
 test('add new feed key', function (t) {
-  t.plan(13)
+  t.plan(10)
 
-  var feed = hypercore(ram)
+  feed = hypercore(ram)
 
   feed.append(['hello', 'world'], function () {
     archives.changes(function (err, changeFeed) {
@@ -23,7 +23,7 @@ test('add new feed key', function (t) {
 
       changeFeed.get(0, function (err, data) {
         t.ifError(err, 'changes feed get error')
-        t.same(changeFeed.blocks, 1, 'one block in change feed')
+        t.same(changeFeed.length, 1, 'one block in change feed')
         data = JSON.parse(data)
         t.same(data.key, feed.key.toString('hex'), 'changes key okay')
         t.same(data.type, 'add', 'changes type add')
@@ -57,72 +57,72 @@ test('add new feed key', function (t) {
   })
 })
 
-// test('add duplicate feed key', function (t) {
-//   t.plan(6)
-//
-//   archives.add(feed.key, function (err) {
-//     t.ifError(err, 'add error')
-//
-//     archives.list(function (err, data) {
-//       t.ifError(err, 'list error')
-//       t.same(data.length, 1, 'archives.list has one key')
-//       t.same(data[0], feed.key, 'archive.list has correct key')
-//     })
-//
-//     archives.changes(function (err, changeFeed) {
-//       t.ifError(err, 'changes error')
-//       t.same(changeFeed.blocks, 1, 'one block still in changeFeed')
-//     })
-//   })
-// })
-//
-// test('replicate to hypercore from archiver', function (t) {
-//   t.plan(4)
-//
-//   var clone = hypercore(ram)
-//
-//   clone.get(0, function (err, data) {
-//     t.ifError(err, 'clone get error')
-//     t.same(data.toString(), 'hello', 'clone receives feed data')
-//   })
-//
-//   clone.get(1, function (err, data) {
-//     t.ifError(err, 'clone get error')
-//     t.same(data.toString(), 'world', 'clone receives feed data')
-//   })
-//
-//   replicate(clone, archives)
-// })
-//
-// test('remove existing key', function (t) {
-//   t.plan(10)
-//
-//   archives.on('remove', function (key) {
-//     t.pass('remove event')
-//     t.same(key, feed.key, 'remove event with right key')
-//   })
-//
-//   archives.remove(feed.key, function (err) {
-//     t.ifError(err, 'remove error')
-//
-//     archives.changes(function (err, changeFeed) {
-//       t.ifError(err, 'changes error')
-//
-//       changeFeed.get(1, function (err, data) {
-//         t.ifError(err, 'change feed get block err')
-//         t.same(changeFeed.blocks, 2, 'two blocks in changeFeed')
-//         data = JSON.parse(data)
-//         t.same(data.type, 'remove', 'change type is removed')
-//         t.same(data.key, feed.key.toString('hex'), 'change has correct key')
-//       })
-//     })
-//
-//     archives.list(function (err, data) {
-//       t.ifError(err, 'list error')
-//       t.same(data.length, 0, 'archives.list does not have any keys')
-//     })
-//   })
-// })
+test('add duplicate feed key', function (t) {
+  t.plan(6)
+
+  archives.add(feed.key, function (err) {
+    t.ifError(err, 'add error')
+
+    archives.list(function (err, data) {
+      t.ifError(err, 'list error')
+      t.same(data.length, 1, 'archives.list has one key')
+      t.same(data[0], feed.key, 'archive.list has correct key')
+    })
+
+    archives.changes(function (err, changeFeed) {
+      t.ifError(err, 'changes error')
+      t.same(changeFeed.length, 1, 'one block still in changeFeed')
+    })
+  })
+})
+
+test('replicate to hypercore from archiver', function (t) {
+  t.plan(4)
+
+  var clone = hypercore(ram)
+
+  clone.get(0, function (err, data) {
+    t.ifError(err, 'clone get error')
+    t.same(data.toString(), 'hello', 'clone receives feed data')
+  })
+
+  clone.get(1, function (err, data) {
+    t.ifError(err, 'clone get error')
+    t.same(data.toString(), 'world', 'clone receives feed data')
+  })
+
+  replicate(clone, archives)
+})
+
+test('remove existing key', function (t) {
+  t.plan(10)
+
+  archives.on('remove', function (key) {
+    t.pass('remove event')
+    t.same(key, feed.key, 'remove event with right key')
+  })
+
+  archives.remove(feed.key, function (err) {
+    t.ifError(err, 'remove error')
+
+    archives.changes(function (err, changeFeed) {
+      t.ifError(err, 'changes error')
+
+      changeFeed.get(1, function (err, data) {
+        t.ifError(err, 'change feed get block err')
+        t.same(changeFeed.length, 2, 'two blocks in changeFeed')
+        data = JSON.parse(data)
+        t.same(data.type, 'remove', 'change type is removed')
+        t.same(data.key, feed.key.toString('hex'), 'change has correct key')
+      })
+    })
+
+    archives.list(function (err, data) {
+      t.ifError(err, 'list error')
+      t.same(data.length, 0, 'archives.list does not have any keys')
+    })
+  })
+})
 
 function replicate (dest, source) {
   var stream1 = dest.replicate()
